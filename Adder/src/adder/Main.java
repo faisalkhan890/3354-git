06bd2e1193cde3c2dca207fc381a954fb470a256
Mohammad Faisal Khan <faisal@Mohammads-MacBook-Pro.local>
2020-09-24 18:19:11 -0500

    Subject: modify file Main.java - made the program to run for n numbers

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -1,17 +1,35 @@
 package adder;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+
         try {
             int result = addArguments(args);
-            System.out.println(result);
+            System.out.println("The sum of the Arbitrary numbers is: " + result);
         } catch (Exception e) {
-            System.err.println("Please provide two integers to add");
+            System.err.println("Please provide the number of positive integer number to add");
         }
     }
 
     private static int addArguments(String[] args) {
-        return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+        int arb,sum=0;
+                  
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the arbitrary number you want sum of: ");
+        arb = sc.nextInt();
+        int a[]=new int[arb]; 
+
+        System.out.println("enter the "+arb+" numbers ");
+        for(int i=0;i<arb;i++)
+        {      
+      System.out.println("enter  number "+(i+1)+":");
+                a[i]=sc.nextInt();
+                sum += a[i];
+        }
+      return sum;
+        //return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
     }
 }
