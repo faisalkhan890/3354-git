5e4f8cce07c833863f227191c71b6f0e572f59e4
Mohammad Faisal Khan <faisal@cometnet-10-21-21-252.utdallas.edu>
2020-09-24 21:54:38 -0500

    Subject: modified Main.java - added exceptions in master branch

--- Adder/src/adder/Main.java
+++ Adder/src/adder/Main.java
@@ -1,4 +1,4 @@
-package adder;
+//package adder;
 import java.util.Scanner;
 
 public class Main {
@@ -6,30 +6,39 @@ public class Main {
     public static void main(String[] args) {
 
 
-        try {
+        try 
+        {
             int result = addArguments(args);
+            if (args.length == 0 ) // checks if arguments are provided and throws error if they are no arguements
+            {
+                throw new ArrayIndexOutOfBoundsException();
+            } 
             System.out.println("The sum of the Arbitrary numbers is: " + result);
-        } catch (Exception e) {
-            System.err.println("Please provide the number of positive integer number to add");
+
+        } 
+        
+        catch (ArrayIndexOutOfBoundsException e) 
+        {
+            System.err.println("Invalid number of arguements/numbers");
+        }
+        catch(NumberFormatException n)
+        {
+            System.out.println("Invalid characters");
         }
     }
 
-    private static int addArguments(String[] args) {
-        int arb,sum=0;
-                  
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the arbitrary number you want sum of: ");
-        arb = sc.nextInt();
-        int a[]=new int[arb]; 
 
-        System.out.println("enter the "+arb+" numbers ");
-        for(int i=0;i<arb;i++)
-        {      
-      System.out.println("enter  number "+(i+1)+":");
-                a[i]=sc.nextInt();
-                sum += a[i];
+    private static int addArguments(String[] args) {
+        
+        int arb =0, sum=0;
+        
+      
+        for(int i= arb ;i<args.length;i++)
+        {
+                sum+=Integer.valueOf(args[i]) ; // for arbitrary positive numbers
+   
         }
-      return sum;
-        //return Integer.valueOf(args[0]) + Integer.valueOf(args[1]);
+
+        return sum;
     }
 }
